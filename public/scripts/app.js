56fdd69321a1893225e6a8dcc9a63861c0b5a13d
Guy Dilena <guydilena@gmail.com>
2022-03-12 18:21:25 -0400

    THEY BE BOUNCING!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,53 +1,102 @@
 // Client facing scripts here
 
   //INTIALIZE MAP TO HOMEPAGE//
-const initMap = function() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 45.95634662125372, lng: -66.63999655179681},
-        zoom: 6
-      });
+  const initMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.95634662125372, lng: -66.63999655179681},
+      zoom: 6
+    });
 
+    google.maps.event.addListener(map, 'click', function(event) {
+      placeMarker(map, event.latLng);
+    });
 
-
-      google.maps.event.addListener(map, "click", (event) => {
-
-        const lat = event.latLng.toJSON().lat;
-        const lng = event.latLng.toJSON().lng;
-        console.log(lat, lng)
-
-      //Add Marker
-      function addMarker(location) {
-        const marker = new google.maps.Marker({
+    function placeMarker(map, location) {
+      var marker = new google.maps.Marker({
         position: location,
-        map,
-        draggable: true
-        
-        })
-      }
-        addMarker(event.latLng, map);
-
+        map: map,
+        animation: google.maps.Animation.BOUNCE,
+        draggable: true,
       });
-
-}
+      var infowindow = new google.maps.InfoWindow({
+        content: 'Latitude: ' + location.lat() +
+        '<br>Longitude: ' + location.lng()
+      });
+      infowindow.open(map,marker);
+    }
+    }
 
 
 const loadMap = function() {
-    $.ajax({
-     method: "GET",
-    url: '/',
-    })
-    .then(res => {
-    initMap();
-    })
-    .catch(err => {
-      console.log('err', err)
-    })
+  $.ajax({
+   method: "GET",
+  url: '/',
+  })
+  .then(res => {
+  initMap();
+  })
+  .catch(err => {
+    console.log('err', err)
+  })
 }
 
 $(document).ready(function() {
 
-  loadMap();
+loadMap();
 
 });
+
+
+// // Client facing scripts here
+
+//   //INTIALIZE MAP TO HOMEPAGE//
+// const initMap = function() {
+//       map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 45.95634662125372, lng: -66.63999655179681},
+//         zoom: 6
+//       });
+
+
+
+//       google.maps.event.addListener(map, "click", (event) => {
+
+//         const lat = event.latLng.toJSON().lat;
+//         const lng = event.latLng.toJSON().lng;
+//         console.log(lat, lng)
+
+//       //Add Marker
+//       function addMarker(location) {
+//         const marker = new google.maps.Marker({
+//         position: location,
+//         map,
+//         draggable: true
+
+//         })
+//       }
+//         addMarker(event.latLng, map);
+
+//       });
+
+// }
+
+
+// const loadMap = function() {
+//     $.ajax({
+//      method: "GET",
+//     url: '/',
+//     })
+//     .then(res => {
+//     initMap();
+//     })
+//     .catch(err => {
+//       console.log('err', err)
+//     })
+// }
+
+// $(document).ready(function() {
+
+//   loadMap();
+
+// });
 
 
