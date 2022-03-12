8b0b531220d7599be61a01790aff6696087e3014
Noah <lauffern@gmail.com>
2022-03-12 12:29:59 +0000

    add: initMap to render homepage map moved to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,35 @@
 // Client facing scripts here
+
+  //INTIALIZE MAP TO HOMEPAGE//
+const initMap = function() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 2
+      });
+
+    }
+
+
+
+const loadMap = function() {
+
+    $.ajax({
+     method: "GET",
+    url: '/',
+    })
+    .then(res => {
+    initMap();
+
+    })
+    .catch(err => {
+      console.log('err', err)
+
+    })
+
+}
+
+$(document).ready(function() {
+
+  loadMap();
+
+});
